cbc93e5e3456be9578f4c826c34cc5bb7d619a15
ahmad35 <ahmad@apptunix.com>
2021-12-03 01:12:13 +0530

    just hiding the code

--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-// import Counters from "./components/counters";
+import Counters from "./components/counters";
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Counters />
   </React.StrictMode>,
   document.getElementById("root")
 );
